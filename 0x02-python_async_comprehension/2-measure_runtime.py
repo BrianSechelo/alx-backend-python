04d1297172de4a2fd7993f77e61cca2a9efecb6d
BrianSechelo <sechbrian123@gmail.com>
2024-08-06 19:42:44 +0000

    Task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module documentation"""
+import asyncio
+import time
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+async def measure_runtime() -> float:
+    """func documentation"""
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time.perf_counter() - start
+    return end    
